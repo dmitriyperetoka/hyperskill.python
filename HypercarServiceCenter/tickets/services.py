9647e829bb3fa0021b457c9f30ecae5bf681c966
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-01-05 03:09:22 +0300

    HypercarServiceCenter: docstrings and refactoring.

--- HypercarServiceCenter/tickets/services.py
+++ HypercarServiceCenter/tickets/services.py
@@ -1,4 +1,9 @@
+from typing import Dict
+
+
 class Ticket:
+    """Node-like object for linked list in SubQueue."""
+
     tickets_issued = 0
 
     def __new__(cls):
@@ -11,6 +16,8 @@ class Ticket:
 
 
 class SubQueue:
+    """Linked list based queue with Ticket objects as nodes."""
+
     def __init__(self, service: str, minutes_per_ticket: int) -> None:
         self.minutes_per_ticket = minutes_per_ticket
         self.service = service
@@ -18,13 +25,18 @@ class SubQueue:
         self.head = None
         self.tail = None
 
+    def __repr__(self) -> str:
+        return self.service
+
     def is_empty(self) -> bool:
+        """Check if the sub-queue is empty."""
         if self.length == 0:
             return True
 
         return False
 
     def push(self, ticket: Ticket) -> None:
+        """Add new ticket to the sub-queue."""
         if self.is_empty():
             self.head = ticket
             self.tail = ticket
@@ -35,6 +47,7 @@ class SubQueue:
         self.length += 1
 
     def pop(self) -> Ticket:
+        """Extract next ticket from the sub-queue."""
         ticket = self.head
         self.length -= 1
 
@@ -46,30 +59,30 @@ class SubQueue:
         return ticket
 
     def estimate_waiting_time(self) -> int:
+        """Estimate time to serve all the tickets in the sub-queue."""
         return self.minutes_per_ticket * self.length
 
 
-class Queue:
-    def __init__(self) -> None:
+SUB_QUEUES: Dict[str, SubQueue] = {
+    'change_oil': SubQueue('Change oil', 2),
+    'inflate_tires': SubQueue('Inflate tires', 5),
+    'diagnostic': SubQueue('Get diagnostic', 30),
+}
+
+
+class PriorityQueue:
+    """Multi-line queue with priority to the sub-queue of less waiting
+    time per ticket.
+    """
+
+    def __init__(self, sub_queues: Dict[str, SubQueue] = None) -> None:
+        if sub_queues is None:
+            sub_queues = SUB_QUEUES
+        self.sub_queues = sub_queues
         self.next_ticket_number = None
-        self.sub_queues = {
-            'change_oil': SubQueue('Change oil', 2),
-            'inflate_tires': SubQueue('Inflate tires', 5),
-            'diagnostic': SubQueue('Get diagnostic', 30),
-        }
-
-    def issue_ticket(self, service: str) -> int:
-        ticket = Ticket()
-        self.sub_queues[service].push(ticket)
-        return ticket.number
-
-    def process(self) -> None:
-        for sub_que in self.sub_queues.values():
-            if not sub_que.is_empty():
-                self.next_ticket_number = sub_que.pop().number
-                break
 
     def estimate_waiting_time(self, new_ticket_service: str) -> int:
+        """Estimate waiting time before issuing a new ticket."""
         estimated_time = 0
 
         for service, sub_queue in self.sub_queues.items():
@@ -79,3 +92,21 @@ class Queue:
                 break
 
         return estimated_time
+
+    def issue_ticket(self, service: str) -> int:
+        """Add a new ticket to the corresponding sub-queue and return
+        the ticket number.
+        """
+        ticket = Ticket()
+        self.sub_queues[service].push(ticket)
+        return ticket.number
+
+    def process(self) -> None:
+        """Select the next ticket to provide service to."""
+        for service, sub_queue in sorted(
+                self.sub_queues.items(),
+                key=lambda item: item[1].minutes_per_ticket
+        ):
+            if not sub_queue.is_empty():
+                self.next_ticket_number = sub_queue.pop().number
+                break
--- HypercarServiceCenter/tickets/views.py
+++ HypercarServiceCenter/tickets/views.py
@@ -5,19 +5,23 @@ from django.http.response import HttpResponse
 from django.template.response import TemplateResponse
 from django.views.generic import TemplateView, View
 
-from .services import SubQueue, Queue
+from .services import PriorityQueue, SubQueue
 
-queue = Queue()
+queue = PriorityQueue()
 
 
 class WelcomeView(View):
-    greeting = '<h2>Welcome to the Hypercar Service!</h2>'
+    """Display greeting message."""
+
+    greeting_message = '<h2>Welcome to the Hypercar Service!</h2>'
 
     def get(self, request: HttpRequest) -> HttpResponse:  # noqa
-        return HttpResponse(self.greeting)
+        return HttpResponse(self.greeting_message)
 
 
 class MenuView(TemplateView):
+    """Display menu page."""
+
     template_name = 'menu.html'
 
     def get_context_data(self) -> Dict[str, ItemsView[str, SubQueue]]:
@@ -25,6 +29,10 @@ class MenuView(TemplateView):
 
 
 class GetTicketView(TemplateView):
+    """Issue new ticket and display its number and estimated
+    waiting time.
+    """
+
     template_name = 'get_ticket.html'
 
     def get_context_data(
@@ -38,7 +46,9 @@ class GetTicketView(TemplateView):
         return context
 
 
-class ProcessView(TemplateView):
+class ProcessingView(TemplateView):
+    """Display the operator menu and process the next ticket."""
+
     template_name = 'processing.html'
 
     def get_context_data(self) -> Dict[str, ValuesView[SubQueue]]:
@@ -50,6 +60,8 @@ class ProcessView(TemplateView):
 
 
 class NextView(TemplateView):
+    """Display the page showing which ticket is next to serve."""
+
     template_name = 'next.html'
 
     def get_context_data(self) -> Dict[str, int]:
